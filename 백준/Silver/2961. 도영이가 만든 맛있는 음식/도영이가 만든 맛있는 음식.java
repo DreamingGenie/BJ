6f71470a88f14f399ce2733940b9eef17cca3197
jin <yusengha@naver.com>
2026-02-03 13:45:49 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 64 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -1,4 +1,5 @@
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,8 +8,11 @@ import java.util.ArrayList;
 public class Main {
     static int N;
     static int[][] list;
-    static ArrayList<Integer> arrlist;
+    //static ArrayList<Integer> arrlist;
     static int answer=Integer.MAX_VALUE;
+    static int sour=1;
+    static int acerbity=0;
+    static int count=0;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
@@ -19,26 +23,23 @@ public class Main {
             list[i][0]=Integer.parseInt(input[0]);
             list[i][1]=Integer.parseInt(input[1]);
         }
-        arrlist=new ArrayList<>();
-        bt(arrlist,0);
+        //arrlist=new ArrayList<>();
+
+        bt(sour,acerbity,0,count);
         System.out.println(answer);
     }
-    public static void bt(ArrayList<Integer> arrlist,int index){
-        if(index==N&& !arrlist.isEmpty()){
-            int sour=1;
-            int acerbity=0;
-            for(int i=0;i<arrlist.size();i++){
-                int temp=arrlist.get(i);
-                sour*=list[temp][0];
-                acerbity+=list[temp][1];
+    public static void bt(int sour, int acerbity,int index,int count){
+        if(index==N) {
+            if (count!=0) {
+                answer = Math.min(answer, (Math.abs(sour - acerbity)));
             }
-            answer=Math.min(answer,(Math.abs(sour-acerbity)));
+            return;
         }
-        for(int i=index;i<N;i++){
-            arrlist.add(i);
-            bt(arrlist,i+1);
-            arrlist.remove(arrlist.indexOf(i));
-            bt(arrlist,i+1);
-        }
+
+
+        bt(sour*list[index][0],acerbity+list[index][1],index+1,count+1);
+
+        bt(sour,acerbity,index+1,count);
+
     }
 }
