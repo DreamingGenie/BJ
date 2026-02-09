74d8bea030e04725f26e42295123e243a187e8f0
jin <yusengha@naver.com>
2026-02-09 16:29:15 +0900

    [Gold IV] Title: Puyo Puyo, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.java
@@ -1,5 +1,3 @@
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -13,12 +11,10 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		input();
 		while(true) {
-			ArrayList<Puyo> list=find();
-			//System.out.println(list.size());
-			if(list.size()==0)
+			if(!find())
 				break;
 			
-			delete(list);
+			Gravity();
 			//printmap();
 			answer++;
 		}
@@ -34,56 +30,51 @@ public class Main {
 			}
 		}
 	}
-	public static ArrayList<Puyo> find() {
-		ArrayList<Puyo> list=new ArrayList<>();
-		Queue<Puyo> bfs=new LinkedList<>();
-		Queue<Puyo> save=new LinkedList<>();
+	public static boolean find() {
+		boolean isfind=false;
 		boolean[][] visited=new boolean[12][6];
 		for(int y=0;y<12;y++) {
 			for(int x=0;x<6;x++) {
-				//System.out.println(answer+" "+y+" "+x);
 				char flag=map[y][x];
-				if(flag=='.')
+				if(flag=='.'||visited[y][x])
 					continue;
-				bfs.clear();
-				save.clear();
+				
+				Queue<Puyo> save=new LinkedList<>();
 				visited[y][x]=true;
-				bfs.offer(new Puyo(x,y));
-				save.offer(new Puyo(x,y));
-				//System.out.println(save.size());
-				while(!bfs.isEmpty()) {
-					
-					Puyo temp=bfs.poll();
-					for(int d=0;d<4;d++) {
-						int nx=temp.x+dx[d];
-						int ny=temp.y+dy[d];
-						//System.out.println(isValid(nx,ny)+"  "+!visited[y][x]);
-						if(isValid(nx,ny)&&map[ny][nx]==flag&&!visited[ny][nx]) {
-							visited[ny][nx]=true;
-							bfs.offer(new Puyo(nx,ny));
-							save.offer(new Puyo(nx,ny));						
-							//System.out.println(save.size());
-						}
-					}
-				}
+				bfs(save,x,y,visited,flag);
+				
 				if(save.size()>=4) {
 					while(!save.isEmpty()) {
-						list.add(save.poll());
-						
+						Puyo temp=save.poll();
+						map[temp.y][temp.x]='.';
 					}
-				}
-				
+					isfind=true;
+				}	
 			}
 		}
-		//System.out.println("11");
 		
-		return list;
-		
+		return isfind;	
+	}
+	public static void bfs(Queue<Puyo> save, int x, int y, boolean[][] visited, char flag) {
+		Queue<Puyo> bfs=new LinkedList<>();
+		bfs.offer(new Puyo(x,y));
+		save.offer(new Puyo(x,y));
+		while(!bfs.isEmpty()) {
+			Puyo temp=bfs.poll();
+			for(int d=0;d<4;d++) {
+				int nx=temp.x+dx[d];
+				int ny=temp.y+dy[d];
+				if(isValid(nx,ny)&&map[ny][nx]==flag&&!visited[ny][nx]) {
+					visited[ny][nx]=true;
+					bfs.offer(new Puyo(nx,ny));
+					save.offer(new Puyo(nx,ny));						
+				}
+			}
+		}
 	}
 	public static void printmap() {
 		for(int y=0;y<12;y++) {
 			for(int x=0;x<6;x++) {
-				//System.out.println(answer+" "+y+" "+x);
 				System.out.print(map[y][x]+" ");
 			}
 			System.out.println();
@@ -92,33 +83,21 @@ public class Main {
 	public static boolean isValid(int x,int y) {
 		return (x>=0&&x<6&&y>=0&&y<12);
 	}
-	public static void delete(ArrayList<Puyo> list) {
-		for(int i=0;i<list.size();i++) {
-			Puyo temp=list.get(i);
-			map[temp.y][temp.x]='.';
-		}
-//		printmap();
-//		System.out.println("+=======");
-		for(int x=0;x<6;x++) {
-			for (int y=11;y>=0;y--) {
-				if(map[y][x]=='.') {
-					boolean early=true;
-					for(int ny=y-1;ny>=0;ny--) {
-						if(map[ny][x]!='.') {
-							early=false;
-							map[y][x]=map[ny][x];
-							map[ny][x]='.';
-							break;
-						}
-//						printmap();
-//						System.out.println("+=======");
-					}
-					if(early)
-						break;
-				}
-			}
-		}
-	}
+	public static void Gravity() {
+        for (int x = 0; x < 6; x++) {
+            Queue<Character> column = new LinkedList<>();
+            for (int y = 11; y >= 0; y--) {
+                if (map[y][x] != '.') {
+                    column.add(map[y][x]);
+                    map[y][x] = '.';
+                }
+            }
+            int index = 11;
+            while (!column.isEmpty()) {
+                map[index--][x] = column.poll();
+            }
+        }
+    }
 	public static class Puyo{
 		int x;
 		int y;
