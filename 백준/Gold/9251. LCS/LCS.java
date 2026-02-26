5ed903df198f62d728cc585453ad4ea412426e4d
jin <yusengha@naver.com>
2026-02-26 12:06:07 +0900

    [Gold IV] Title: LCS, Time: 96 ms, Memory: 15992 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,26 +1,43 @@
+
+
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        solve();
-    }
-    public static void solve() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String A = br.readLine();
-        String B = br.readLine();
-        int[][] LCS = new int[A.length()+1][B.length()+1];
-        for(int i=1;i<=A.length();i++){
-            for(int j=1;j<=B.length();j++){
-                if(A.charAt(i-1)==B.charAt(j-1)){
-                    LCS[i][j]=LCS[i-1][j-1]+1;
-                }
-                else{
-                    LCS[i][j]=Math.max(LCS[i-1][j],LCS[i][j-1]);
-                }
+	public static void main(String args[]) throws Exception
+    {
+
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int T=1;
+//        T=Integer.parseInt(br.readLine());
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            char[] arrA=st.nextToken().toCharArray();
+            st=new StringTokenizer(br.readLine());
+            char[] arrB=st.nextToken().toCharArray();
+            int lenA=arrA.length;
+            int lenB=arrB.length;
+            int answer=0;
+            int[][] dp=new int[lenA+1][lenB+1];
+            for(int a=1;a<=lenA;a++) {
+            	for(int b=1;b<=lenB;b++) {
+            		if(arrA[a-1]==arrB[b-1]) {
+            			dp[a][b]=dp[a-1][b-1]+1;
+            		}
+            		else {
+            			dp[a][b]=Math.max(dp[a-1][b],dp[a][b-1]);
+            		}
+            	}
             }
+            //System.out.println("#"+test_case+" "+dp[lenA][lenB]);
+            System.out.println(dp[lenA][lenB]);
         }
-        System.out.println(LCS[A.length()][B.length()]);
     }
+	//
 }
