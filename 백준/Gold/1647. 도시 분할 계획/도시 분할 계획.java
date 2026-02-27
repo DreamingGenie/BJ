2a055151bb64c81a27d2c7a402ff65cdfa9dcb28
jin <yusengha@naver.com>
2026-02-27 14:34:46 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 1244 ms, Memory: 324460 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -0,0 +1,83 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	static PriorityQueue<Edge> edgelist=new PriorityQueue<>();
+	static int N;
+	static int M;
+	static int[] parent;
+	static int answer=0;
+	public static void main(String[] args) throws IOException{
+		input();
+		if(N==2) {
+			System.out.println(0);
+			return;
+		}
+		solve();
+		System.out.println(answer);
+	}
+	public static void solve() {
+		int edgeCount = 0;
+		while(!edgelist.isEmpty()) {
+			Edge temp=edgelist.poll();
+			int l=temp.dotA;
+			int r=temp.dotB;
+			int lparent=find(l);
+			int rparent=find(r);
+			if(lparent==rparent)
+				continue;
+			else {
+				answer+=temp.value;
+				parent[lparent]=rparent;
+				edgeCount++;
+			}
+			if(edgeCount==N-2)
+				break;
+		}
+		
+	}
+	public static int find(int A) {
+		if(parent[A]==A)
+			return A;
+		return parent[A]=find(parent[A]);
+	}
+	public static void input() throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		parent=new int[N+1];
+		for(int i=1;i<=N;i++) {
+			parent[i]=i;
+		}
+		for(int i=0;i<M;i++) {
+			st=new StringTokenizer(br.readLine());
+			int A=Integer.parseInt(st.nextToken());
+			int B=Integer.parseInt(st.nextToken());
+			int val=Integer.parseInt(st.nextToken());
+			edgelist.add(new Edge(A,B,val));
+		}
+	}
+	
+	static class Edge implements Comparable<Edge>{
+		int dotA;
+		int dotB;
+		int value;
+		public Edge(int A, int B, int val) {
+			dotA=A;
+			dotB=B;
+			value=val;
+		}
+		
+		@Override
+		public int compareTo(Edge other) {
+			return Integer.compare(this.value, other.value);
+		}
+
+	}
+}
