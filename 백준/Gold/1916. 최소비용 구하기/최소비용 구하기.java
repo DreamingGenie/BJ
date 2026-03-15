7707f5d788aeb27431cdba01df21ff2be810b957
jin <yusengha@naver.com>
2026-03-15 21:09:17 +0900

    [Gold V] Title: 최소비용 구하기, Time: 392 ms, Memory: 51460 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,78 +1,75 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     static int N;
     static int M;
-    static ArrayList<Edge>[] graph;
+    static int[][] map;
     static int[] dist;
-    static int answer=Integer.MAX_VALUE;
-    public static void main(String[] args) throws IOException {
-        solve();
-    }
-    public static void solve() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        M = Integer.parseInt(br.readLine());
-        graph =new ArrayList[N+1];
-        dist = new int[N+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        for (int i = 0; i <= N; i++) {
-            graph[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= M; i++) {
-            String[] input = br.readLine().split(" ");
-            int start = Integer.parseInt(input[0]);
-            int end = Integer.parseInt(input[1]);
-            int value = Integer.parseInt(input[2]);
-            graph[start].add(new Edge(end,value));
-        }
-        String[] input = br.readLine().split(" ");
-        int start = Integer.parseInt(input[0]);
-        int end = Integer.parseInt(input[1]);
+    static int start;
+    static int end;
+    static ArrayList<Edge>[] adj;
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    static final int INF=Integer.MAX_VALUE/10;
+    public static void main(String[] args) throws Exception{
+        input();
+        PriorityQueue<Edge> pq=new PriorityQueue<>();
+        pq.add(new Edge(start,0));
         dist[start]=0;
-        pq.add(new Node(start,dist[start]));
-        while (!pq.isEmpty()) {
-            Node now = pq.poll();
-            if (now.value > dist[now.loc]) {
-                continue;
-            }
-            for (Edge e: graph[now.loc]) {
-                int next=e.goal;
-                int nextvalue=e.value+now.value;
-                if (dist[next]>nextvalue) {
-                    dist[next]=nextvalue;
-                    pq.add(new Node(next,dist[next]));
+        while(!pq.isEmpty()){
+            Edge temp=pq.poll();
+            int now=temp.to;
+            int cost=temp.cost;
+
+            if(cost>dist[now]) continue;
+
+            for (Edge next : adj[now]) {
+                if (dist[next.to] > dist[now] + next.cost) {
+                    dist[next.to] = dist[now] + next.cost;
+                    pq.add(new Edge(next.to, dist[next.to]));
                 }
             }
         }
         System.out.println(dist[end]);
     }
-    public static class Edge {
-        int goal;
-        int value;
-        public Edge(int goal, int value) {
-            this.goal = goal;
-            this.value = value;
-        }
-    }
-    public static class Node implements Comparable<Node>{
-        int loc;
-        int value;
-        public Node(int loc, int value) {
-            this.loc = loc;
-            this.value = value;
-        }
-        @Override
-        public int compareTo(Node o) {
-            return this.value - o.value;
-        }
-    }
+    public static void input() throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine());
+        M=Integer.parseInt(br.readLine());
 
+        adj = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) adj[i] = new ArrayList<>();
+        dist=new int[N+1];
+        Arrays.fill(dist,INF);
+
+        StringTokenizer st;
+        for(int i=1;i<=M;i++){
+            st=new StringTokenizer(br.readLine());
+            int from=Integer.parseInt(st.nextToken());
+            int to=Integer.parseInt(st.nextToken());
+            int cost=Integer.parseInt(st.nextToken());
+            adj[from].add(new Edge(to, cost));
+
+        }
+        st=new StringTokenizer(br.readLine());
+        start = Integer.parseInt(st.nextToken());
+        end = Integer.parseInt(st.nextToken());
+    }
+    public static class Edge implements Comparable<Edge>{
+        int to;
+        int cost;
+        public Edge(int to, int cost){
+            this.to=to;
+            this.cost=cost;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.cost-o.cost;
+        }
+    }
 }
